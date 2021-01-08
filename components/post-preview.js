c16c2db734fe13c1b90a22413e7378e0f28f3110
yukidaruma <yukidaruma@users.noreply.github.com>
2021-01-09 07:23:55 +0900

    feat: Make `coverImage` and `excerpt` optional

--- components/post-preview.js
+++ components/post-preview.js
@@ -1,27 +1,15 @@
-import Avatar from '../components/avatar'
-import DateFormatter from '../components/date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
+import Link from 'next/link';
+import DateFormatter from '../components/date-formatter';
+import CoverImage from './cover-image';
 
-export default function PostPreview({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}) {
+export default function PostPreview({ title, excerpt, coverImage, date, slug }) {
   return (
     <div>
-      <div className="mb-5">
-        <CoverImage
-          slug={slug}
-          title={title}
-          src={coverImage}
-          height={278}
-          width={556}
-        />
-      </div>
+      {coverImage && (
+        <div className="mb-5">
+          <CoverImage slug={slug} title={title} src={coverImage} height={278} width={556} />
+        </div>
+      )}
       <h3 className="text-3xl mb-3 leading-snug">
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a className="hover:underline">{title}</a>
@@ -30,8 +18,7 @@ export default function PostPreview({
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      {excerpt && <p className="text-lg leading-relaxed mb-4">{excerpt}</p>}
     </div>
-  )
+  );
 }
